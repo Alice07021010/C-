834e98c541132d90ae1d77cd7d3cfb276352a41f
Alice07021010 <126603838+Alice07021010@users.noreply.github.com>
2025-02-21 09:03:40 +0300

    Add files via upload

---
+++ практос 2 по си шарп.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Rectangle
+{
+	// Параметры прямоугольника
+	private double width;
+	private double height;
+
+	// Конструктор по умолчанию
+	public Rectangle()
+	{
+		width = 1.0;
+		height = 1.0;
+	}
+
+	// Конструктор с параметрами
+	public Rectangle(double width, double height)
+	{
+		this.width = width;
+		this.height = height;
+	}
+
+	// Метод для вычисления площади
+	public double Area()
+	{
+		return width * height;
+	}
+
+	// Свойства для получения и установки ширины и высоты
+	public double Width
+	{
+		get { return width; }
+		set { width = value; }
+	}
+
+	public double Height
+	{
+		get { return height; }
+		set { height = value; }
+	}
+}
+
+// Пример использования класса
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Введите ширину прямоугольника:");
+		double width = Convert.ToDouble(Console.ReadLine());
+
+		Console.WriteLine("Введите высоту прямоугольника:");
+		double height = Convert.ToDouble(Console.ReadLine());
+
+		Rectangle rect = new Rectangle(width, height);
+		Console.WriteLine($"Площадь прямоугольника: {rect.Area()}");
+	}
+}
